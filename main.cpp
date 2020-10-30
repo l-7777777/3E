a249563ec8e661caa10721165d49edb568c1fe91
l-7777777 <lysander.mealy@gmail.com>
2020-10-30 19:24:01 -0400

    Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
-using std::cout;
+#include <fstream>
+#include <string>
+#include <streambuf>
+using namespace std;
+
+void Main();
 
 int main() {
-    cout << "Hello, world!" << std::endl;
+    try {
+        Main();
+        return 0;
+    } catch (exception e) {
+        cout << e.what() << endl;
+        return -1;
+    }
+}
+
+void Main() {
+    std::ifstream t("/mnt/c/Users/lysan/Donwloads/codeTags.html");
+    std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+    printf("%s\n", str.c_str());
+    return;
 }
