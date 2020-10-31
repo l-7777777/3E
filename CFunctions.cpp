fafce24f9f89f9bb3a09c4e506b020c0ba30b6a0
l-7777777 <lysander.mealy@gmail.com>
2020-10-31 11:20:40 -0400

    Commit

--- CFunctions.cpp
+++ CFunctions.cpp
@@ -1,12 +1,23 @@
 #include "CFunctions.hpp"
+
 namespace CStr {
     char *split(char *str, char delimiter, int max) {
         char *toReturn[max];
         for (int i = 0; i < sizeof(str); i++) {
+            int curPos = 0;
             if (str[i] == delimiter) {
-                
+                toReturn[curPos] = substr(str, 0, i);
+                curPos++;
             }
         }
         return *toReturn;
+    }
+
+    char *substr(char *str, int start, int end) {
+        char *toReturn;
+        for (int i = start; i < (end + 1); i++) {
+            toReturn[i - start] = str[i];
+        }
+        toReturn[sizeof(toReturn)] = 0x00;
     }
 }
