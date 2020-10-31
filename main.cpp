9cf06d026f08d852c90344668c2697a26c63b53d
l-7777777 <lysander.mealy@gmail.com>
2020-10-31 08:31:50 -0400

    Commit

--- main.cpp
+++ main.cpp
@@ -3,32 +3,21 @@
 #include <string>
 #include <streambuf>
 using namespace std;
-//check Operating system
-#ifdef linux //check linux and import Unix file libraries 
-    //linux libraries
-    #endif
-#ifdef _WIN32 //import required windows libraries
-    //windows libraries
-    #endif
 
+void Main() {
+    std::ifstream t("/mnt/c/Users/lysan/Donwloads/codeTags.html");
+    std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+    printf("%s\n", str.c_str());
+    cout << "Method is working" << endl;
+    return;
+}
 
 
 int main() {
-
-
     try {
         Main();
     } catch (exception e){
         cout << e.what() << endl;
     }
-    cout << "int Main worked \n" << endl;
-
-
-    }
-void Main() {
-    std::ifstream t("/mnt/c/Users/lysan/Donwloads/codeTags.html");
-    std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
-    printf("%s\n", str.c_str());
-    cout << "Method is working \n";
-    return;
+    cout << "Main worked" << endl;
 }
