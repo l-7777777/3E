36a203b20663c775562468f7deb397a366966366
plasmarad <userofgogle356@gmail.com>
2020-10-30 20:55:50 -0400

    commit

--- main.cpp
+++ main.cpp
@@ -9,16 +9,17 @@ void Main();
 int main() {
     try {
         Main();
-        return 0;
     } catch (exception e) {
         cout << e.what() << endl;
-        return -1;
     }
-}
+    cout << "int Main worked" << endl;
 
+
+    }
 void Main() {
     std::ifstream t("/mnt/c/Users/lysan/Donwloads/codeTags.html");
     std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
     printf("%s\n", str.c_str());
+    cout << "Method is working ";
     return;
 }
