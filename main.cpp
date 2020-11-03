38880ccec7cce92d32d8c662d3757b3576796cc2
plasmarad <userofgogle356@gmail.com>
2020-11-02 20:14:07 -0500

    something

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 #include "info.hpp"
 #include "CFunctions.hpp"
+#include <string.h>
+
 
 int main(int argc, char *argv[]) {
+    cout << "Main.cpp runned! \n";
     *argv = CStr::split(argv[0], ' ');
-    cout << argv[1] << '\a' << '\n';
+    for (int i = 0; i < argc; i++){
+        cout << argv[i] << endl;
+    }
 }
